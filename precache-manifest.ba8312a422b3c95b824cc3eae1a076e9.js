95d9756b3e640c1284a51cd15003a67b871760ff
Sebastian Hewelt <30390585+sebahewelt@users.noreply.github.com>
2019-09-28 17:58:15 +0200

    Testowy_projekt

---
+++ precache-manifest.ba8312a422b3c95b824cc3eae1a076e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05a135cc98c2323a038b4b20a7435ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd287c7989077bbf0eb8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "63cb2bdab47c4b55b3a3",
+    "url": "/static/js/2.5232bb64.chunk.js"
+  },
+  {
+    "revision": "cd287c7989077bbf0eb8",
+    "url": "/static/js/main.ed1536bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
